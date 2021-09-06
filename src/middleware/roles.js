1a7909a42d7d5636b815211aead1b05f37cc137e
Waseem Mansha Gondal <waseem.mansha@gmail.com>
2021-09-06 07:23:11 -0700

    add unit test

--- src/middleware/roles.js
+++ src/middleware/roles.js
@@ -10,10 +10,14 @@ const checkRole =
   (...roles) =>
   (req, res, next) => {
     if (!req.user) {
+      // return res.status(401).send('you are not logged in');
       return apiResponse(res, 'error', 'You are not logged in', 401);
     }
     const isAllowed = roles.find((role) => req.user.role === role);
     if (!isAllowed) {
+      // return res
+      //   .status(403)
+      //   .json({ error: 'You are not allowed to make this request.' });
       return apiResponse(res, 403, 'You are not allowed to access this route');
     }
 

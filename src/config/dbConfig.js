92f70d40dfb2e76ecafadaf7faa8c59f8d362456
Waseem Mansha Gondal <waseem.mansha@gmail.com>
2021-09-09 10:39:27 +0500

    change db configs accrding to production

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,31 +1,28 @@
-const dotenv = require('dotenv').config().parsed;
-if (dotenv) {
-  module.exports = {
-    development: {
-      username: dotenv.DB_USERNAME,
-      password: dotenv.DB_PASSWORD,
-      database: dotenv.DB_NAME,
-      host: '127.0.0.1',
-      dialect: 'postgres',
-      // operatorsAliases: false,
-    },
-    test: {
-      username: dotenv.DB_USERNAME_TEST,
-      password: dotenv.DB_PASSWORD_TEST,
-      database: dotenv.DB_NAME_TEST,
-      host: '127.0.0.1',
-      dialect: 'postgres',
-      // operatorsAliases: false,
-    },
-    production: {
-      username: dotenv.DB_USERNAME,
-      password: dotenv.DB_PASSWORD,
-      database: dotenv.DB_NAME,
-      host: '127.0.0.1',
-      dialect: 'postgres',
-      // operatorsAliases: false,
-    },
-  };
-} else {
-  throw new Error('.env file is missing');
-}
+/*eslint-disable */
+require('dotenv').config();
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    // operatorsAliases: false,
+  },
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    // operatorsAliases: false,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    // operatorsAliases: false,
+  },
+};
